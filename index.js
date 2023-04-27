9cb38a5fa7bc1f0ef8ec7c9d8be1c34d6bd83abe
emir4169 <89991965+emir4169@users.github.com>
2023-04-27 13:34:38 +0300

    oh no

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const prompt = require('prompt');
+binarydata = 101
+binarylistdata = []
 
 prompt.start();
-
 prompt.get(['binary'], function (err, result) {
     if (err) { return onErr(err); }
-    console.log('Command-line input received:' + binary);
+    binarydata = result.binary
+    console.log('Command-line input received:' + binarydata);
+    binarylistdata = String(num).split('').map(str => Number(str));
+
 });
 
 function onErr(err) {
